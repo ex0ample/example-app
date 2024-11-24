f034d5cc32b20552a1403340af6f3dec4b9f40d2
mis <mis@email.com>
2024-11-24 19:10:33 +0700

    update code day 3

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @stack('head')
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Styles / Scripts -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+
+    
+    <style>
+    body {
+        font-family: "Noto Sans Thai", sans-serif !important;
+        font-optical-sizing: auto;
+        font-style: normal;
+    }
+    .strike_none {
+        text-decoration: none;
+    }
+    .green {
+        background-color: green !important;
+    }
+    </style>
+</head>
+
+<body class="font-sans antialiased" data-theme="light">
+    <main class="flex justify-center items-center min-h-screen bg-[#f9f9f9]">
+        <div class="h-auto w-96 bg-white rounded-lg p-4 bg-[#f9f9f9]">
+            <div class="header">
+                <div class="mt-3 text-sm text-[#8ea6c8] flex justify-between items-center">
+                    <p class="set_date">Thursday 1 Jan</p>
+                    <p class="set_time">00:00 AM</p>
+                </div>
+            </div>
+
+            @yield('main')
+
+        </div>
+    </main>
+
+    <script>
+    var setdate = document.querySelector(".set_date");
+    var settime = document.querySelector(".set_time");
+
+    var date = new Date().toDateString();
+    setdate.innerHTML = date;
+
+    setInterval(function() {
+        var time = new Date().toLocaleTimeString();
+        settime.innerHTML = time;
+    }, 500);
+    </script>
+    
+    @stack('footer')
+
+</body>
+
+</html>
---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.master')
+
+@push('head')
+<title>Add Todo </title>
+@endpush
+
+@section('main')
+<div class="todo">
+    <h2 class="text-xl font-semibold mt-5 text-[#063c76]">To-do List</h2>
+    <ul class="my-4 ">
+        @for ($i = 0; $i < 8; $i++) 
+        <li class=" mt-4" id="{{ $i }}">
+            <div class="flex justify-start items-center h-12 shadow drop-shadow-md rounded-[7px] px-3 hover:shadow-md">
+                <div class="grow flex justify-start items-center">
+                    <span id="check{{ $i }}" class=" w-7 h-7 bg-white rounded-full border border-white transition-all cursor-pointer hover:border-[#36d344] flex justify-center items-center" onclick="checked({{ $i }})"><i class="text-gray-300 fa fa-check"></i></span>
+                    <strike id="strike{{ $i }}" class="strike_none text-sm ml-4 text-[#5b7a9d] font-semibold">ทำการบ้านภาษาไทย </strike>
+                    <span class="ml-3 mt-0 text-sm text-[#5b7a9d]">12:00 PM</span>
+                </div>
+                <div class="flex-none">
+                    <button id="dropdownButton" data-dropdown-toggle="dropdown-{{ $i }}" data-dropdown-placement="left-end" data-dropdown-offset-distance="10" data-dropdown-offset-skidding="0" class="inline-block text-gray-400 focus:ring-4 focus:outline-none focus:ring-gray-100 rounded-lg text-sm p-1.5" type="button">
+                        <span class="sr-only">Open dropdown</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                            <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                        </svg>
+                    </button>
+                    <!-- Dropdown menu -->
+                    <div id="dropdown-{{ $i }}" class="z-20 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+                        <ul class="py-2" aria-labelledby="dropdownButton">
+                            <li>
+                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">รายละเอียด</a>
+                            </li>
+                            <li>
+                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">เเก้ไข</a>
+                            </li>
+                            <li>
+                                <a href="#" class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100">ลบ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </li>
+        @endfor
+    </ul>
+    <a href="{{ route('todo.create') }}" class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">เพิ่มรายการ</a>
+</div>
+@endsection
+
+@push('footer')
+<script>
+function checked(id) {
+    var checked_green = document.getElementById("check" + id);
+    var strike_unstrike = document.getElementById("strike" + id);
+
+    strike_unstrike.classList.toggle('strike_none');
+    checked_green.classList.toggle('green');
+}
+</script>
+@endpush
